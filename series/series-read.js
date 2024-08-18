7554f5282b5c0bb8d0eda4a7e29f2945c3285de5
jackmuva <33766184+jackmuva@users.noreply.github.com>
2024-08-18 11:55:26 -0700

    added get endpoint

---
+++ series/series-read.js
@@ -0,0 +1,43 @@
+import { DynamoDB } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
+
+const dynamo = DynamoDBDocument.from(new DynamoDB());
+
+/**
+ * Demonstrates a simple HTTP endpoint using API Gateway. You have full
+ * access to the request and response payload, including headers and
+ * status code.
+ *
+ * To scan a DynamoDB table, make a GET request with the TableName as a
+ * query string parameter. To put, update, or delete an item, make a POST,
+ * PUT, or DELETE request respectively, passing in the payload to the
+ * DynamoDB API as a JSON body.
+ */
+export const handler = async (event) => {
+    let body;
+    let statusCode = '200';
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+
+    try {
+        switch (event.httpMethod) {
+            case 'GET':
+                body = await dynamo.scan({ TableName: "trtlpost-series" });
+                break;
+            default:
+                throw new Error(`Unsupported method "${event.httpMethod}"`);
+        }
+    } catch (err) {
+        statusCode = '400';
+        body = err.message;
+    } finally {
+        body = JSON.stringify(body);
+    }
+
+    return {
+        statusCode,
+        body,
+        headers,
+    };
+};
